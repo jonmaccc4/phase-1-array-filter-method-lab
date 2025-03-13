c4b104c31788a1878948f32abeb5550a533902fc
jondev <jonmac2454@gmail.com>
2025-03-13 21:54:33 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+ 
+  const drivers = ["Bobby", "Sammy", "bobby", "Sarah"];
+  console.log(findMatching(drivers, "bobby")); 
+
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+
+  const driver = ["Bobby", "Sammy", "bobby", "Sarah", "Bo"];
+  console.log(fuzzyMatch(drivers, "Bo")); 
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
